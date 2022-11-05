c46a1e8d715bff7d9dec11b403e00d72a6ce3d8e
BCAS-Student18 <BCAS-Student18@DESKTOP-2AGPVOD>
2022-11-05 13:40:56 +0800

    Add project files.

---
+++ paint_JavieDeLeon/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace paint_JavieDeLeon
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var IPensilTool = new PencilTool();
+            IPensilTool.MouseUp();
+            IPensilTool.MouseOver();
+            var Erase = new Square();
+            Erase.MouseOver();
+            Console.ReadLine();
+        }
+    }
+
+    public interface IPaintTool
+    {
+        void MouseUp();
+        void MouseOver();
+    }
+
+    public class PencilTool : IPaintTool
+    {
+        public void MouseOver()
+        {
+            Console.WriteLine("Show pencil icon");
+        }
+
+        public void MouseUp()
+        {
+            Console.WriteLine("Draw a line.");
+        }
+    }
+
+    public class Square : IPaintTool
+    {
+        public void MouseOver()
+        {
+            Console.WriteLine("Show square icon");
+        }
+
+        public void MouseUp()
+        {
+            Console.WriteLine("Draw a square.");
+        }
+    }
+
+    public class Eraser : IPaintTool
+    {
+        public void MouseOver()
+        {
+            Console.WriteLine("Show an eraser icon");
+        }
+
+        public void MouseUp()
+        {
+            Console.WriteLine("Erase the selected object.");
+        }
+    }
+
+}
